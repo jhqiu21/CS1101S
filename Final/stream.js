33714b4d71521f7d23ca17a808e2f594af79ca8e
Source Academy User <QIU JINHANG>
2023-11-19 21:59:48 +0800

    Changes made from Source Academy

--- Final/stream.js
+++ Final/stream.js
@@ -1,4 +1,17 @@
 // Stream 
+
+function ones_stream() {
+    return pair(1, ones_stream);
+}
+
+function enum_stream(low, high) {
+    return low > high
+        ? null
+        : pair(low, () => enum_stream(low + 1, high));
+}
+
+
+
 function make_alternating_stream(s) {
     return is_null(s)
         ? null
@@ -10,13 +23,13 @@ function make_alternating_stream(s) {
                                     stream_tail(stream_tail(s)))));
 }
 
-
-
-
 function skip_stream(s) {
     
 // takes an infinite stream as argument and returns an infinite 
 // stream that contains only every second element of the given stream.
-    
     return pair(head(s), () => skip_stream(stream_tail(stream_tail(s))));
+}
+
+function partial_sum(s) {
+    
 }
