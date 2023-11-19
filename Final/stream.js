e1361c02bd11124b9940639845a27367882cc696
Source Academy User <QIU JINHANG>
2023-11-19 22:37:00 +0800

    Changes made from Source Academy

--- Final/stream.js
+++ Final/stream.js
@@ -26,8 +26,8 @@ function eval_stream(s, n) {
         : pair(head(s), eval_stream(stream_tail(s), n - 1));
 }
 
-function fib_stream(a, b) {
-    return pair(a, () => fib_stream(b, a + b));
+function fib_stream_ver1(a, b) {
+    return pair(a, () => fib_stream_ver1(b, a + b));
 }
 
 function more(a, b) {
@@ -37,7 +37,46 @@ function more(a, b) {
         : pair(a, () => more(a + 1, b));
 }
 
-function replace()
+function replace(s, a, b) {
+    // create a new stream by replacing in a given stream a 
+    // particular value by another value
+    return is_null(s) 
+        ? null
+        : pair(head(s) === a ? a : b, () => replace(stream_tail(s), a, b));
+}
+
+function list_to_inf_stream(xs) {
+    function helper(ys) {
+        return is_null(ys)
+            ? helper(ys)
+            : pair(head(ys), () => list_to_inf_stream(tail(ys)));
+    }
+    return is_null(xs)
+        ? null
+        : helper(xs);
+}
+
+function add_streams(s1, s2) {
+    if (is_null(s1)) {
+        return s2;
+    } else if (is_null(s2)) {
+        return s1;
+    } else {
+        return pair(head(s1) + head(s2), 
+                    () => add_streams(stream_tail(s1), 
+                                      stream_tail(s2)));
+    }
+}
+// Important, cook it with memorization
+const fib_stream_ver2 = pair(0, 
+                            pair(1,
+                                () => add_streams(stream_tail(fib_stream_ver2),
+                                                  fib_stream_ver2)));
+
+
+
+
+
 
 function stream_map(f, s) {
     return is_null(s)
