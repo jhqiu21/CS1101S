0d2aeff0a99eb2f974b835434e30608643798f5e
Source Academy User <QIU JINHANG>
2023-11-20 22:40:15 +0800

    Changes made from Source Academy

--- S11_inclass.js
+++ S11_inclass.js
@@ -22,6 +22,26 @@ const five = ints(1, 5);
 eval_stream(stream_pairs(five), 10);
 
 // Q1(c)
+const integers = pair(1, () => integers); // An Infinite Stream
+// const s2 = stream_pairs(integers); // Runs forever
 
-const integers = pair(1, () => integers);
- const s2 = stream_pairs(integers);
+// Q1(d)
+
+function stream_append_pickle(xs, ys) {
+    return is_null(xs)
+        ? ys()
+        : pair(head(xs), () => stream_append_pickle(stream_tail(xs), ys));
+}
+
+function stream_pairs2(s) {
+    return is_null(s)
+        ? null
+        : stream_append_pickle(
+            stream_map(
+                sn => pair(head(s), sn),
+                stream_tail(s)),
+            () => stream_pairs(stream_tail(s)));
+}
+
+const s3 = stream_pairs2(integers); // 
+// eval_stream(s3, 10);
