0d1d65aab41ee041f7f3e3dced0b5529bbcc9758
Source Academy User <QIU JINHANG>
2023-11-20 21:27:04 +0800

    Changes made from Source Academy

--- S6_inclass.js
+++ S6_inclass.js
@@ -1,3 +1,6 @@
+
+// CS1101S In-class Studio Sheet 9
+
 function count_pairs(x) {
     if (!is_pair(x)) {
         return 0;
@@ -6,4 +9,18 @@ function count_pairs(x) {
     }
 }
 
+// Output: 3
+let ver1 = list(0, 0, 0); 
+display(count_pairs(ver1));
 
+// Output: 4
+let four = pair(null, null);
+let inte = pair(four, four);
+let ver2 = pair(inte, null);
+display(count_pairs(ver2));
+
+// Output: 7
+let sev = pair(null, null);
+let inte_1 = pair(sev, sev);
+let ver3 = pair(inte_1, inte_1);
+display(count_pairs(ver3));
