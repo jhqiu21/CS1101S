22de844d796542f6db158651c5000b2a41a16bff
Source Academy User <QIU JINHANG>
2023-11-20 23:01:07 +0800

    Changes made from Source Academy

--- S11_inclass.js
+++ S11_inclass.js
@@ -50,3 +50,32 @@ function stream_pairs2(s) {
 
 const s3 = stream_pairs2(integers);
 // eval_stream(s3, 10);
+
+// Q1(e)
+
+// Note that we can never actually reach the second arguement, because
+// the first argument stream is infinite
+
+
+function interleave_stream_append(s1, s2) {
+    // We do not care the order of the elements, we can combine two streams
+    // in an interleaving manner
+    return is_null(s1)
+        ? s2
+        : pair(head(s1), () => interleave_stream_append(s2, tail(s1)));
+}
+
+function stream_pairs3(s) {
+    return is_null(s) || is_null(stream_tail(s)) 
+        ? null
+        : pair(pair(head(s), head(stream_tail(s))),
+                () => interleave_stream_append(
+                        stream_map(x => pair(head(s), x),
+                                    stream_tail(stream_tail(s))),
+                        stream_pairs3(stream_tail(s))));
+}
+
+const s4 = stream_pairs3(integers);
+// eval_stream(s4, 10);
+
+
