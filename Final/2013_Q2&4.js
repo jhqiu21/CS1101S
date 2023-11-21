c4055fd181a7404ff77771fd01af6faafd7fa3b7
Source Academy User <QIU JINHANG>
2023-11-21 19:58:32 +0800

    Changes made from Source Academy

--- Final/2013_Q2&4.js
+++ Final/2013_Q2&4.js
@@ -14,15 +14,48 @@ function three(x) {
     return x + 3;
 }
 
+// Q2(a)
 
+function function_to_number(fun) {
+    return fun(0);
+}
 
+function number_to_function(num) {
+    return x => num + x;
+}
 
+// function_to_number(number_to_function(3));
 
+// Q2(b)
 
+function functional_plus(fun1, fun2) {
+    return x => fun1(fun2(x));
+}
 
+//function_to_number(functional_plus(one, two));
 
+// Q2(c)
 
+function minus_one(f) { 
+    /* pre-decleared */ 
+    return number_to_function(function_to_number(f) - 1);
+}
 
+function is_zero(f) {
+    /* pre-decleared */
+    return function_to_number(f) === 0;
+}
+
+function functional_times(fun1, fun2) {
+    if (is_zero(fun1) || is_zero(fun2)) {
+        return x => x;
+    } else {
+        return functional_plus(fun2, functional_times(minus_one(fun1), fun2));
+    }
+}
+
+// function_to_number(functional_times(two, three));
+// function_to_number(functional_times(minus_one(three), minus_one(three)));
 
 
 // Q4 Cutting and Pasting
